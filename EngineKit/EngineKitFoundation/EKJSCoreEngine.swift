aac64413f6d054d3976208a2a02a361d7c8d4db3
vinivendra <vinivendra@gmail.com>
2016-05-11 12:18:42 -0400

    Added swift subscripts to JSContext and JSValue.

--- EngineKit/EngineKitFoundation/EKJSCoreEngine.swift
+++ EngineKit/EngineKitFoundation/EKJSCoreEngine.swift
@@ -13,14 +13,28 @@ extension Console: ConsoleExport {
 	}
 }
 
+extension JSValue {
+	@nonobjc subscript(name: String) -> AnyObject {
+		get { return self.objectForKeyedSubscript(name) }
+		set(newValue) { self.setObject(newValue, forKeyedSubscript: name) }
+	}
+}
+
+extension JSContext {
+	@nonobjc subscript(name: String) -> AnyObject {
+		get { return self.objectForKeyedSubscript(name) }
+		set(newValue) { self.setObject(newValue, forKeyedSubscript: name) }
+	}
+}
+
 public class EKJSCoreEngine: EKLanguageEngine {
 	let context = JSContext()
 
 	public init() {
 		context.exceptionHandler = { context, value in
-			let stackTrace = value.objectForKeyedSubscript("stack").toString()
-			let lineNumber = value.objectForKeyedSubscript("line").toInt32()
-			let column = value.objectForKeyedSubscript("column").toInt32()
+			let stackTrace = value["stack"].toString()
+			let lineNumber = value["line"].toInt32()
+			let column = value["column"].toInt32()
 			let moreInfo = "in method \(stackTrace)\nLine number \(lineNumber), column \(column)"
 			print("JAVASCRIPT ERROR: \(value) \(moreInfo)")
 		}
@@ -30,10 +44,10 @@ public class EKJSCoreEngine: EKLanguageEngine {
 		} as @convention(block) (JSValue)->()
 		let printObj = unsafeBitCast(printFunc, AnyObject.self)
 
-		context.setObject(printObj, forKeyedSubscript: "print")
-		context.setObject(printObj, forKeyedSubscript: "alert")
+		context["print"] = printObj
+		context["alert"] = printObj
 
-		context.setObject(Console(), forKeyedSubscript: "console")
+		context["console"] = Console()
 	}
 
 	public func runScript(filename filename: String) {
