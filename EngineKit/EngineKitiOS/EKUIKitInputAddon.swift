417e1e71379b104f985d9681f386da67752fe1b0
vinivendra <vinivendra@gmail.com>
2016-05-16 17:41:44 -0400

    Added other gesture recognizers.

--- EngineKit/EngineKitiOS/EKUIKitInputAddon.swift
+++ EngineKit/EngineKitiOS/EKUIKitInputAddon.swift
@@ -9,11 +9,27 @@ public class EKUIKitInputAddon: EKEventAddon {
 	public init(view: UIView) {
 		self.view = view
 
-		let tapSelector = #selector(EKUIKitInputAddon.handleTap(_:))
-		let tapGestureRecognizer = UITapGestureRecognizer(target: self,
-		                                                  action: tapSelector)
+		let tapGestureRecognizer = UITapGestureRecognizer(
+			target: self, action: #selector(EKUIKitInputAddon.handleTap(_:)))
 		self.view.addGestureRecognizer(tapGestureRecognizer)
 
+		let panGestureRecognizer = UIPanGestureRecognizer(
+			target: self, action: #selector(EKUIKitInputAddon.handlePan(_:)))
+		self.view.addGestureRecognizer(panGestureRecognizer)
+
+		let pinchGestureRecognizer = UIPinchGestureRecognizer(
+			target: self, action: #selector(EKUIKitInputAddon.handlePinch(_:)))
+		self.view.addGestureRecognizer(pinchGestureRecognizer)
+
+		let rotationGestureRecognizer = UIRotationGestureRecognizer(
+			target: self,
+			action: #selector(EKUIKitInputAddon.handleRotation(_:)))
+		self.view.addGestureRecognizer(rotationGestureRecognizer)
+
+		let longPressGestureRecognizer = UILongPressGestureRecognizer(
+			target: self,
+			action: #selector(EKUIKitInputAddon.handleLongPress(_:)))
+		self.view.addGestureRecognizer(longPressGestureRecognizer)
 	}
 
 	public func setup(onEngine engine: EKEngine) throws {
@@ -22,5 +38,21 @@ public class EKUIKitInputAddon: EKEventAddon {
 
 	@objc public func handleTap(gestureRecognizer: UIGestureRecognizer) {
 		print("tap!")
+	}
+
+	@objc public func handlePan(gestureRecognizer: UIGestureRecognizer) {
+		print("pan!")
+	}
+
+	@objc public func handlePinch(gestureRecognizer: UIGestureRecognizer) {
+		print("pinch!")
+	}
+
+	@objc public func handleRotation(gestureRecognizer: UIGestureRecognizer) {
+		print("rotation!")
+	}
+
+	@objc public func handleLongPress(gestureRecognizer: UIGestureRecognizer) {
+		print("long press!")
 	}
 }
